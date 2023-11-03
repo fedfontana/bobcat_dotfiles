54eb4d1c459317361f715a2c5786925f88b6cca8
fedfontana <federico.fontana2000@gmail.com>
2023-11-03 15:36:13 +0100

    changed keymaps

--- nvim/.config/nvim/lua/fedfontana/plugins/copilot.lua
+++ nvim/.config/nvim/lua/fedfontana/plugins/copilot.lua
@@ -1,4 +1,20 @@
 return {
 	"github/copilot.vim",
-	event = { "BufReadPre", "BufNewFile" },
+	event = "VeryLazy",
+	init = function()
+		vim.g.copilot_no_tab_map = true
+	end,
+	config = function()
+		local s = vim.keymap.set
+		s("i", "<C-c>", [[copilot#Accept("\<CR>")]], {
+			silent = true,
+			expr = true,
+			script = true,
+			replace_keycodes = false,
+		})
+
+		s("n", "<leader>cc", vim.cmd.Copilot, {
+			desc = "Show copilot completions for current buffer",
+		})
+	end,
 }
--- nvim/.config/nvim/lua/fedfontana/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/fedfontana/plugins/harpoon.lua
@@ -1,24 +1,28 @@
 return {
-  "ThePrimeagen/harpoon",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+	"ThePrimeagen/harpoon",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local keymap = vim.keymap
+		local hm = require("harpoon.mark")
+		local ui = require("harpoon.ui")
 
-    keymap.set(
-      "n",
-      "<leader>hm",
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      { desc = "Mark file with harpoon" }
-    )
-    keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-    keymap.set(
-      "n",
-      "<leader>hp",
-      "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
-  end,
+		keymap.set("n", "<leader>hm", hm.add_file, { desc = "Mark file with harpoon" })
+		keymap.set("n", "<leader>hn", ui.nav_next, { desc = "Go to next harpoon mark" })
+		keymap.set("n", "<leader>hp", ui.nav_prev, { desc = "Go to previous harpoon mark" })
+		keymap.set("n", "<C-h>", ui.toggle_quick_menu, { desc = "Show harpoon files" })
+		keymap.set("n", "<C-a>", function()
+			ui.nav_file(1)
+		end, { desc = "Go to first harpoon file" })
+		keymap.set("n", "<C-s>", function()
+			ui.nav_file(2)
+		end, { desc = "Go to second harpoon file" })
+		keymap.set("n", "<C-f>", function()
+			ui.nav_file(3)
+		end, { desc = "Go to third harpoon file" })
+		keymap.set("n", "<C-g>", function()
+			ui.nav_file(4)
+		end, { desc = "Go to fourth harpoon file" })
+	end,
 }
