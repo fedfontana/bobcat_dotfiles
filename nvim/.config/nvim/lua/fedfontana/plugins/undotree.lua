4da05611dc2935e54d56c8b852ddcf1219a59f49
fedfontana <federico.fontana2000@gmail.com>
2025-05-25 21:18:44 +0200

    other uncommitted changes

--- nvim/.config/nvim/lua/fedfontana/plugins/undotree.lua
+++ nvim/.config/nvim/lua/fedfontana/plugins/undotree.lua
@@ -9,7 +9,7 @@ return {
 			desc = "Toggle undotree",
 		},
 	},
-	config = function()
+	init = function()
 		if vim.fn.has("persistent_undo") == 1 then
 			vim.opt.undodir = vim.fn.expand("~/.config/nvim/.undodir")
 			vim.opt.undofile = true
--- nvim/.config/nvim/lua/fedfontana/plugins/which-key.lua
+++ nvim/.config/nvim/lua/fedfontana/plugins/which-key.lua
@@ -2,9 +2,14 @@ return {
 	"folke/which-key.nvim",
 	enabled = true,
 	event = "VeryLazy",
+	opts = {
+		delay = 400,
+		filter = function(mapping)
+			return mapping.desc ~= "Disable space (leader) in normal mode"
+		end,
+	},
 	init = function()
 		vim.o.timeout = true
 		vim.o.timeoutlen = 500
 	end,
-	opts = {},
 }
